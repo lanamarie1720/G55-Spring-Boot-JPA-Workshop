436703aca0140b867712b93f4f7cea26de18fe24
LanaMB <lanamarie1720@gmail.com>
2025-06-10 10:50:59 +0200

    Creation of entity package. Creation of class and entities AppUser and Details

---
+++ src/main/java/entity/AppUser.java
@@ -0,0 +1,20 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class AppUser {
+    @Id
+    private Long id;
+
+}
